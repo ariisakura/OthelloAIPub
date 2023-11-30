ad531b8126a48d251ea1d071f6448524a2596535
ariisakura <152460155+ariisakura@users.noreply.github.com>
2023-11-30 12:00:59 +0900

    Update OthelloAction.py

--- OthelloAction.py
+++ OthelloAction.py
@@ -1,10 +1,15 @@
 import random
 from typing import List
 
-
+isActiveMode = false
+turnCnt = 0
 def getAction(board, moves) -> List[int]:
     # 自分のターンにフロントエンドから呼ばれるメソッド
-    return select_random_moves(moves)
+    if turnCnt > 30:
+        isActiveMode = true
+        return isActiveMode
+	else:
+        return isActiveMode
 
 
 def select_random_moves(moves) -> List[int]:
